fd192a8063b3d34ebd6500fa4a814079add31f31
vitor.dev <146399948+its-vitor@users.noreply.github.com>
2023-11-13 00:43:33 -0300

    Add files via upload

---
+++ lib/src/request_handler.py
@@ -0,0 +1,14 @@
+import requests
+
+class RequestHandler:
+    def __init__(self) -> None:
+        pass
+    
+    async def send_post(self):
+        pass
+
+    async def send_get(self):
+        pass
+
+    async def send_delete(self):
+        pass
